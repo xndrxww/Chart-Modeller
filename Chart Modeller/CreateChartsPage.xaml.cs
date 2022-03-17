399fa1165d1b80c0fe8547279eee96da10adda06
xndrxww <isip_a.s.kuznecov@mpt.ru>
2022-03-17 16:43:01 +0300

    вывод таблиц и данных при создании графика

--- Chart Modeller/CreateChartsPage.xaml.cs
+++ Chart Modeller/CreateChartsPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,6 +10,8 @@ namespace Chart_Modeller
     public partial class CreateChartsPage : Page
     {
         private string ChartName;
+        private string connectionString = $@"Data Source=SPH-Monitoring;Initial Catalog=master;Persist Security Info=True;User ID=sa;Password=111111@N";
+
         public CreateChartsPage(string pageName)
         {
 
@@ -19,20 +23,26 @@ namespace Chart_Modeller
         {
             MainWindow.PageName.Text = ChartName;
 
-            //string connectionString = $@"Data Source={serverTxt.Text};Initial Catalog=master;Persist Security Info=True;User ID={loginTxt.Text};Password={passwordTxt.Text}";
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    try
-            //    {
-            //        connection.Open();
-            //        Serialization();
-            //        MainWindow.MainFrameInstance.Navigate(new PanelsPage());
-            //    }
-            //    catch (SqlException)
-            //    {
-            //        MessageBox.Show("Не удалось подключиться к серверу");
-            //    }
-            //}
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var table = new DataTable();
+                new SqlDataAdapter("select TABLE_NAME from DBAtools.INFORMATION_SCHEMA.TABLES", connection).Fill(table);
+
+                tablesBox.ItemsSource = table.DefaultView;
+                //tablesBox.SelectedIndex = dbBox.Items.Count - 1;
+            }
+        }
+
+        private void tablesBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var table = new DataTable();
+                new SqlDataAdapter($"select TOP(30) * from DBAtools.dbo.{tablesBox.SelectedValue.ToString()}", connection).Fill(table);
+
+                dataGrid.ItemsSource = table.DefaultView;
+                //tablesBox.SelectedIndex = dbBox.Items.Count - 1;
+            }
         }
     }
 }
